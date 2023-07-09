9558c9d76e122a9ccf8fdee4d99131edf83960d8
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-09 22:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+const button = document.getElementById("button");
+
+button.addEventListener("click",(e)=>{
+	const form =document.getElementById("form");
+	e.preventDefault();
+	const h3 =document.getElementById("url");
+	h3.innerHTML="https://localhost:8080/";
+	const name=document.getElementById("name").value;
+	const year=document.getElementById("year").value;
+
+	if(name!=""&&year!=""){
+		h3.innerHTML+=`?name=${name}&year=${year}`;
+	}
+	else if(name==""){
+		h3.innerHTML+=`?year=${year}`
+	}
+    else{  h3.innerHTML+=`?name=${name}`
+
+    }
+    form.reset();
+});
+
+function fetchUrl(){
+	
+}
